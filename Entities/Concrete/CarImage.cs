1eb22326844fcd0878c4da7b4f1efb8b62ac0ee6
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2021-05-10 03:01:07 +0300

    13.Hafta

---
+++ Entities/Concrete/CarImage.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class CarImage : IEntity
+    {
+        int CarImageId { get; set; }
+        int CarId { get; set; }
+        string ImagePath { get; set; }
+        DateTime Date { get; set; }
+
+    }
+}
